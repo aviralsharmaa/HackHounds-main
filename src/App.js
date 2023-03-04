eea0bd7d13241d333f49dec4594883f67cf3a677
Aviral_Sharma <aviral2020it015@abesit.edu.in>
2023-03-04 13:12:38 +0530

    Adding Footer

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Footer from './Components/Footer.js';
+function App() {
+  return (
+    <Footer />
+    // <div>
+    //   <h1> Mainder Ji </h1>
+    // </div>
+  );
+}
+
+export default App;
